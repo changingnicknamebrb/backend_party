1b4a7999006a6e45126ade593579cb5650a0808c
darkitself <47476735+darkitself@users.noreply.github.com>
2020-06-20 16:41:50 +0500

    SQL Templates

--- server.py
+++ server.py
@@ -1,6 +1,22 @@
 import asyncio
+import pymysql
 from aiohttp import web
+#Работа с SQL
+con = pymysql.connect('localhost', 'root', 
+    'root', 'test', cursorclass=pymysql.cursors.DictCursor)
 
+with con:
+    cur = con.cursor()
+    cur.execute("INSERT INTO `users` (`idusers`, 'name') VALUES ({0}, {1});".format('1234', 'John'))         #Добавить запись в таблицу
+    cur.execute("SELECT * FROM users")                                                                       #Выбрать все записи из таблицы. * - получить все столбцы, иначе в '' пишем название столбца. Получить определённые записи - WHERE *условие*
+    rows = cur.fetchall()                                                                                    #
+    for row in rows:                                                                                         #
+        print(row['idusers'], '   ', row['name'])                                                            #
+    cur.execute("UPDATE `users` SET `name` = 'Now I'm Victor' WHERE `id` = 1234;")                           #Изменяем значение в таблице users в столбце name с id = 1234
+    cur.execute("DELETE FROM `users` WHERE `idusers`= 123")                                                  #Удаляем ряд в таблице users с id = 123
+
+
+#Работа с HTTP-requests
 async def hello1(request):
     return web.Response(text="Hello, world1")
 
