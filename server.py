42e76dd8663d2f8fa175642d03dd091a45d9a832
changingnicknamebrb <vasyasin.dmitrii@gmail.com>
2020-06-21 03:32:54 +0500

    cors

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
 import asyncio
 import pymysql
+import aiohttp_cors
 from aiohttp import web
 
 #Работа с SQL
-con = pymysql.connect('localhost', 'root', 'root', 'test', cursorclass=pymysql.cursors.DictCursor)
+con = pymysql.connect('localhost', 'coolname', 'cool', 'one', cursorclass=pymysql.cursors.DictCursor)
 
 with con:
     cur = con.cursor()
@@ -17,10 +18,10 @@ with con:
 
 
 #Работа с HTTP-requests
-async def autorize(request):
+async def getProfile(request):
     with con:
         cur = con.cursor()
-        data = await request.post()
+        data = request.query
         cur.execute("SELECT * FROM usersinfo WHERE `id` = {}".format(data['id']))
         rows = cur.fetchall()
         if rows.__len__() != 0:
@@ -58,24 +59,66 @@ async def createEvent(request):
         cur = con.cursor()
         data = await request.post()
         cur.execute(
-            "INSERT INTO `usersinfo` (`id`, `firstname`, `lastname`, `height`, `weight`, `information`, `birth_date`) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}');".format(
-                data['id'], data['firstname'], data['lastname'], data['height'], data['weight'], data['information'], data['birth_date']))
+            "INSERT INTO `events` (`users`, `creator`, `name`, `description`, `track_id`) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}');".format(
+                data['users'], data['creator'], data['name'], data['description'], data['track_id']))
         return web.json_response({'success': True})
 
 async def changeEvent(request):
-    return web.Response(text="Hello, world2")
+    with con:
+        cur = con.cursor()
+        data = await request.post()
+        for item in data:
+            if item != 'id':
+                cur.execute("UPDATE `events` SET `{0}` = '{1}' WHERE `id` = {2};".format(item, data[item], data['id']))
+        return web.json_response({'success': True})
 
 async def getEvents(request):
     return web.Response(text="Hello, world2")
 
 async def removeEvent(request):
-    return web.Response(text="Hello, world2")
+    with con:
+        cur = con.cursor()
+        data = request.query
+        cur.execute("DELETE FROM `events` WHERE `id`= {}".format(data['id']))
+        return web.json_response({'success': True})
 
 
 app = web.Application()
-app.add_routes([web.post('/autorize', autorize)])
+
+cors = aiohttp_cors.setup(app, defaults={
+    "*": aiohttp_cors.ResourceOptions(
+            allow_credentials=True,
+            expose_headers="*",
+            allow_headers="*",
+        )
+})
+
+resource = cors.add(app.router.add_resource("/autorize"))
+cors.add(resource.add_route("GET", getProfile))
+resource = cors.add(app.router.add_resource("/getProfile"))
+cors.add(resource.add_route("GET", getProfile))
+resource = cors.add(app.router.add_resource("/registrate"))
+cors.add(resource.add_route("POST", registrate))
+resource = cors.add(app.router.add_resource("/changeProfile"))
+cors.add(resource.add_route("POST", changeProfile))
+resource = cors.add(app.router.add_resource("/createEvent"))
+cors.add(resource.add_route("POST", createEvent))
+resource = cors.add(app.router.add_resource("/changeEvent"))
+cors.add(resource.add_route("POST", changeEvent))
+resource = cors.add(app.router.add_resource("/getEvents"))
+cors.add(resource.add_route("GET", getEvents))
+resource = cors.add(app.router.add_resource("/removeEvent"))
+cors.add(resource.add_route("POST", removeEvent))
+
+
+app.add_routes([web.get('/autorize', getProfile)])
+app.add_routes([web.get('/getProfile', getProfile)])
 app.add_routes([web.post('/registrate', registrate)])
 app.add_routes([web.post('/changeProfile', changeProfile)])
+app.add_routes([web.post('/createEvent', createEvent)])
+app.add_routes([web.post('/changeEvent', changeEvent)])
+app.add_routes([web.get('/getEvents', getEvents)])
+app.add_routes([web.post('/removeEvent', removeEvent)])
 
 
-web.run_app(app)
+web.run_app(app, host = '141.101.196.166', port=8080)
